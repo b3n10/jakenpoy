ab256ec0c77a3b8f21f8e4be461866e9d5b29b9d
b3n10 <francisben.lleve@gmail.com>
2017-08-31 09:44:45 +0800

    Convert to OOP and Rename files

--- JackEnPoy.java
+++ JackEnPoy.java
@@ -1,64 +1,79 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.util.InputMismatchException;
 
 public class JackEnPoy {
-  public static void main(String[] args) {
-    /*
-     * rock > scissor
-     * scissor > paper
-     * paper > rock
-     */
-    String[] handMove = {"Rock", "Paper", "Scissor"};
-    Scanner scan = new Scanner(System.in);
-    Random rand = new Random();
-    int user_input;
-    int cp_num, win = 0;
-    boolean game = true;
+  private String[] handMove = {
+    "Rock", 
+    "Paper", 
+    "Scissor"
+  };
+  private int wins = 0;
+  private Random rand = new Random();
+  private Scanner scan = new Scanner(System.in);
+  private boolean gameOver = false;
 
+  public void startGame() {
+    //Welcome message
     System.out.println("Rock Paper Scissors Game !");
 
-    while (game) {
-      try {
+    while (!gameOver) {
+      System.out.print("\nSelect move [0 - Rock, 1 - Paper, 2 - Scissor]: ");
+      gameOver = compareMove( getUserInput(), computerInput() );
+    }
+  }
 
+  public int getUserInput() throws InputMismatchException {
+    int user_input;
+
+    try {
+      user_input = scan.nextInt();
+
+      //if input is more than 2 or negative
+      while (user_input >= 3 || user_input < 0) {
+        System.out.println("Please select only from 0-2.");
         System.out.print("\nSelect move [0 - Rock, 1 - Paper, 2 - Scissor]: ");
         user_input = scan.nextInt();
-
-        while ( user_input >= 3 ) {
-          System.out.print("\nPlease select from 0-2 only!\n\nSelect move [0 - Rock, 1 - Paper, 2 - Scissor]: ");
-          user_input = scan.nextInt();
-        }
-
-        cp_num = rand.nextInt(3) + 0;//will return 0 - 2 random numbers
-        System.out.println("YOU (" + handMove[user_input] + ")" + " VS CP (" + handMove[cp_num] + ")");
-
-        handMove[user_input] = handMove[user_input];
-        handMove[cp_num] = handMove[cp_num];
-
-        //compare move
-        if (handMove[user_input] == handMove[cp_num]) {
-          System.out.println("\n--It's a draw!--");
-        } else if (handMove[user_input] == "Rock" && handMove[cp_num] == "Scissor") {
-          win++;
-          System.out.println("\n--Win " + win + "--");
-        } else if (handMove[user_input] == "Scissor" && handMove[cp_num] == "Paper") {
-          win++;
-          System.out.println("\n--Win " + win + "--");
-        } else if (handMove[user_input] == "Paper" && handMove[cp_num] == "Rock") {
-          win++;
-          System.out.println("\n--Win " + win + "--");
-        } else {
-          System.out.println("You lose!");
-          System.out.println("\nYour final score is " + win);
-          game = false;
-        }
-
-      } catch (Exception e) {
-        System.out.println("Oops, error on input:\n\"" + e + "\"");
-        break;
       }
-    }
-    
 
+      return user_input;
+    } catch (InputMismatchException e) {
+      //stop the whole game if input is not int
+      throw new InputMismatchException();
+    }
 
   }
+
+  public int computerInput() {
+    //random from 0 - 2
+    return rand.nextInt(3) + 0;
+  }
+
+  public boolean compareMove(int x, int y) {
+
+    //print user selected move and computer's move
+    System.out.println("YOU (" + handMove[x] + ")" + " VS CP (" + handMove[y] + ")");
+
+    if (handMove[x] == handMove[y]) {
+      System.out.println("\n--It's a draw!--");
+    } else if (handMove[x] == "Rock" && handMove[y] == "Scissor") {
+      wins++;
+      System.out.println("\n--Win " + wins + "--");
+    } else if (handMove[x] == "Scissor" && handMove[y] == "Paper") {
+      wins++;
+      System.out.println("\n--Win " + wins + "--");
+    } else if (handMove[x] == "Paper" && handMove[y] == "Rock") {
+      wins++;
+      System.out.println("\n--Win " + wins + "--");
+    } else {
+      System.out.println("\n--You lose!--");
+      System.out.println("\nYour final score is " + wins);
+      return true;
+    }
+
+    //continue playing until not lose
+    return false;
+
+  }
+
 }
